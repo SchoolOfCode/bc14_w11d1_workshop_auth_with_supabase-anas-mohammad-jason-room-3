bff6f81d9cf688a0e1c7f7000b00a505705fa267
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-22 16:40:48 +0100

    sign up and login working

--- supabase/src/App.js
+++ supabase/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// Import necessary packages and modules
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+// Create a Supabase client object using the Supabase URL and API key
+const supabase = createClient(
+	'https://xspqvvczdswhmunxfrpg.supabase.co',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzcHF2dmN6ZHN3aG11bnhmcnBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ3NjgwNTUsImV4cCI6MjAwMDM0NDA1NX0.9wDTrAdm7thoXTwrAe-whdP9yohzhQtVwFSMUpCEg10'
+);
+// Define the main App component
+export default function App() {
+	// Define a state variable to represent the user's session
+	const [session, setSession] = useState(null);
+	// Use the useEffect hook to fetch the user's session and set it to the state variable
+	useEffect(() => {
+		supabase.auth.getSession().then(({ data: { session } }) => {
+			setSession(session);
+		});
+		// Create a subscription to the user's authentication state changes
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((_event, session) => {
+			setSession(session);
+		});
+		// Clean up the subscription when the component unmounts
+		return () => subscription.unsubscribe();
+	}, []);
+	// If there is no session, render the authentication UI component
+	if (!session) {
+		return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+	}
+	// If there is a session, render the "Logged in!" message
+	else {
+		return <div>Logged in!</div>;
+	}
 }
-
-export default App;
